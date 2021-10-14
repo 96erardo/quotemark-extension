2942b1f98ed33d5e77c87811fa7c6b47d89d77d8
Gerardo García <gerardoagl96@gmail.com>
2021-10-14 15:50:03 -0500

    Context menu item to store a quote

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,35 @@
 chrome.action.onClicked.addListener(() => {
   chrome.runtime.openOptionsPage();
 })
+
+chrome.identity.onSignInChanged.addListener((_, signedIn) => {
+  chrome.contextMenus.update('store', {
+    enabled: signedIn
+  })
+})
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.identity.getAuthToken({ interactive: false }, (token) => {
+    if (chrome.runtime.lastError && !token) {
+      chrome.contextMenus.create({
+        id: 'store',
+        title: 'Save quote',
+        type: 'normal',
+        contexts: ['selection'],
+        enabled: false
+      })
+
+    } else {
+      chrome.contextMenus.create({
+        id: 'store',
+        title: 'Save quote',
+        type: 'normal',
+        contexts: ['selection']
+      })
+    }
+  })
+})
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  
+})
